62a12f132d39e7bcdc7b8adc82856eb606d0dba0
kartikdiwan <71651448+kartikdiwan@users.noreply.github.com>
2021-11-26 18:00:45 +0530

    Update main.c

--- main.c
+++ main.c
@@ -6,14 +6,19 @@
 #include <sys/types.h>
 #include <dirent.h>
 
+char command[128];
+
+void invalidCommand();
+void cdCommand();
 void listCommand();
-void listPrivCommand();
 void helpCommand();
 void main()
 {
-    char command[128];
-    printf("\nCommand line interface started, enter \"help\" for all available commands \n");
-
+    system("clear");
+    printf("\033[1m");
+    printf("Command line interface started! Enter \"help\" for all available commands");
+    printf("\033[0m");
+    printf("\n\n");
     //get current working directory
     char cwd[1024];
     char hostname[1024];
@@ -21,28 +26,22 @@ void main()
     {
         // print current user in yellow
         getcwd(cwd, sizeof(cwd));
-        printf("\033[1;33m");
+        printf("\033[1m");
         gethostname(hostname, sizeof(hostname));
         printf("%s@%s:", getlogin(), hostname);
         printf("\033[0m");
-        printf("\033[0;31m");
+        printf("\033[0;34;1m");
         printf("\x1b[1m"); 
         printf("%s> ", cwd);
         printf("\x1b[0m");
         printf("\033[0m");
-
         // read command from user
         gets(command);
-        printf("\n");
-        if (strcmp(command, "list") == 0)
+        if (strcmp(command, "list") == 0 || strncmp(command, "list -", 6) == 0)
         {
             listCommand();
         }
-        else if (strcmp(command, "list -priv") == 0)
-        {
-            listPrivCommand(".", 0);
-        }
-        else if (strcmp(command, "help") == 0 || strcmp(command, "info") == 0)
+        else if (strcmp(command, "help") == 0)
         {
             helpCommand();
         }
@@ -54,53 +53,95 @@ void main()
         {
             system("clear");
         }
-        else if(strcmp(command, "exit") == 0 || strcmp(command, "quit") == 0)
-        {
-            exit(0);
-        }
         // implement change directory command
         else if (strncmp(command, "cd", 2) == 0 && command[2] == ' ')
         {
-            // split command into tokens
-            char *token = strtok(command, " ");
-            token = strtok(NULL, " ");
-            if (token == NULL)
+            cdCommand();
+        }
+        else if(strcmp(command, "") == 0)
+        {
+
+        }
+        else
+        {
+            invalidCommand();
+        }
+
+    } while (strcmp(command, "cliquit") != 0);
+}
+
+void invalidCommand()
+{
+    printf("\033[1;31m");
+    printf("Invalid command, enter \"help\" for all available commands \n");
+    printf("\033[0m");
+}
+
+void cdCommand()
+{
+    // split command into tokens
+    char *token = strtok(command, " ");
+    token = strtok(NULL, " ");
+    if (token == NULL)
+    {
+        printf("\033[1;31m");
+        printf("No directory specified\n");
+        printf("\033[0m");
+    }
+    else
+    {
+        // check if directory exists
+        struct stat st;
+        if (stat(token, &st) == 0)
+        {
+            if (S_ISDIR(st.st_mode))
             {
-                printf("No directory specified\n");
+                chdir(token);
             }
             else
             {
-                // check if directory exists
-                struct stat st;
-                if (stat(token, &st) == 0)
-                {
-                    if (S_ISDIR(st.st_mode))
-                    {
-                        chdir(token);
-                    }
-                    else
-                    {
-                        printf("%s is not a directory\n", token);
-                    }
-                }
-                else
-                {
-                    printf("%s does not exist\n", token);
-                }
+                printf("\033[1;31m");
+                printf("%s is not a directory\n", token);
+                printf("\033[0m");
             }
         }
         else
         {
             printf("\033[1;31m");
-            printf("Invalid command, enter \"help\" for all available commands \n");
+            printf("%s does not exist\n", token);
             printf("\033[0m");
         }
-
-    } while (strcmp(command, "cliquit") != 0 || strcmp(command, "quit") != 0 || strcmp(command, "exit") != 0);
+    }
 }
 
 void listCommand()
 {
+    int priv = 0, inode = 0;
+    if(strlen(command)>6 || strlen(command) ==4)
+    {
+        for(int i=6;i<strlen(command); i++)
+        {
+            if(command[i]=='p')
+            {
+                priv = 1;
+            }
+            else if(command[i]=='n')
+            {
+                inode = 1;
+            }
+            else
+            {
+                invalidCommand();
+                return;
+            }
+        }
+    }
+    else
+    {
+        invalidCommand();
+        return;
+    }
+
     DIR *dp;
     struct dirent *entry;
     struct stat statbuf;
@@ -110,18 +151,38 @@ void listCommand()
         exit(1);
     }
     chdir(".");
-    while (entry = readdir(dp))
+    while ((entry = readdir(dp)) != NULL)
     {
+        if(strcmp(entry->d_name,".")==0||strcmp(entry->d_name,"..")==0)
+            continue;
         lstat(entry->d_name, &statbuf);
-        printf("%s\t\t\t", entry->d_name);
-        printf("\n"); 
+        printf("%-16s\t", entry->d_name);
+        if(priv)
+        {
+            printf((S_ISDIR(statbuf.st_mode)) ? "d" : "-");
+            printf((statbuf.st_mode & S_IRUSR) ? "r" : "-");
+            printf((statbuf.st_mode & S_IWUSR) ? "w" : "-");
+            printf((statbuf.st_mode & S_IXUSR) ? "x" : "-");
+            printf((statbuf.st_mode & S_IRGRP) ? "r" : "-");
+            printf((statbuf.st_mode & S_IWGRP) ? "w" : "-");
+            printf((statbuf.st_mode & S_IXGRP) ? "x" : "-");
+            printf((statbuf.st_mode & S_IROTH) ? "r" : "-");
+            printf((statbuf.st_mode & S_IWOTH) ? "w" : "-");
+            printf((statbuf.st_mode & S_IXOTH) ? "x\t" : "-\t");
+        }
+        if(inode)
+        {
+            printf("%ld\t", entry->d_ino);
+        }
+        printf("\n");  
     }
-    printf("\n");
+    closedir(dp);
 }
 
 // function to print the documentation for the project by reading CLIHelp.txt
 void helpCommand()
 {
+    system("clear");
     FILE *fp;
     char line[128];
     fp = fopen("CLIHelp.txt", "r");
@@ -135,45 +196,4 @@ void helpCommand()
         printf("%s", line);
     }
     fclose(fp);
-}
-
-void listPrivCommand(char *dir, int depth)
-{
-    DIR *dp;
-    struct dirent *entry;
-    struct stat statbuf;
-    if ((dp = opendir(dir)) == NULL)
-    {
-        fprintf(stderr, "Cannot Open Directory: %s\n", dir);
-        return;
-    }
-    chdir(dir);
-    while ((entry = readdir(dp)) != NULL)
-    {
-        lstat(entry->d_name, &statbuf);
-        if (S_ISDIR(statbuf.st_mode))
-        {
-            if (strcmp(".", entry->d_name) == 0 || strcmp("..", entry->d_name) == 0)
-                continue;
-            printf("%*s%s\n", depth, " ", entry->d_name);
-            listPrivCommand(entry->d_name, depth + 4);
-        }
-        else
-        {
-            printf("%*s", depth, " ");
-            printf((S_ISDIR(statbuf.st_mode)) ? "d" : "-");
-            printf((statbuf.st_mode & S_IRUSR) ? "r" : "-");
-            printf((statbuf.st_mode & S_IWUSR) ? "w" : "-");
-            printf((statbuf.st_mode & S_IXUSR) ? "x" : "-");
-            printf((statbuf.st_mode & S_IRGRP) ? "r" : "-");
-            printf((statbuf.st_mode & S_IWGRP) ? "w" : "-");
-            printf((statbuf.st_mode & S_IXGRP) ? "x" : "-");
-            printf((statbuf.st_mode & S_IROTH) ? "r" : "-");
-            printf((statbuf.st_mode & S_IWOTH) ? "w" : "-");
-            printf((statbuf.st_mode & S_IXOTH) ? "x" : "-");
-            printf("\t%s", entry->d_name);
-            printf("\n");
-        }
-    }
-    closedir(dp);
 }
