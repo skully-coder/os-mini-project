7b44e1046045dc1be7a40f6be2c171d5d202491e
Abhinav <abhinav.hfs@gmail.com>
2021-11-30 00:25:29 +0530

    added grep and rm functions

--- main.c
+++ main.c
@@ -7,13 +7,17 @@
 #include <sys/types.h>
 #include <dirent.h>
 
-char command[128];
+char command[1024];
 
 void invalidCommand();
-void printerror(char* errmsg);
+void printerror(char *errmsg);
 void cdCommand();
 void listCommand();
 void helpCommand();
+void makelink();
+void removelink();
+void removeFile();
+void grepUtil();
 void main()
 {
     system("clear");
@@ -21,7 +25,7 @@ void main()
     printf("Command line interface started! Enter \"help\" for all available commands");
     printf("\033[0m");
     printf("\n\n");
-    //get current working directory
+    // get current working directory
     char cwd[1024];
     char hostname[1024];
     do
@@ -33,7 +37,7 @@ void main()
         printf("%s@%s:", getlogin(), hostname);
         printf("\033[0m");
         printf("\033[0;34;1m");
-        printf("\x1b[1m"); 
+        printf("\x1b[1m");
         printf("%s> ", cwd);
         printf("\x1b[0m");
         printf("\033[0m");
@@ -47,11 +51,29 @@ void main()
         {
             helpCommand();
         }
+        else if (strncmp(command, "link", 4) == 0 || strncmp(command, "link -", 6) == 0)
+        {
+            makelink();
+        }
+        // implement basic rm command
+        else if (strncmp(command, "rm", 2) == 0 || strncmp(command, "rm -", 4) == 0)
+        {
+            removeFile();
+        }
+        // implement grep function
+        else if (strncmp(command, "grep", 4) == 0 || strncmp(command, "grep -", 6) == 0)
+        {
+            grepUtil();
+        }
+        else if (strncmp(command, "unlink", 6) == 0)
+        {
+            removelink();
+        }
         else if (strcmp(command, "cliquit") == 0)
         {
             system("clear");
         }
-        else if(strcmp(command, "clr") == 0)
+        else if (strcmp(command, "clr") == 0)
         {
             system("clear");
         }
@@ -59,10 +81,6 @@ void main()
         else if (strncmp(command, "chdir", 5) == 0 && command[5] == ' ')
         {
             cdCommand();
-        }
-        else if(strcmp(command, "") == 0)
-        {
-
         }
         else
         {
@@ -77,11 +95,78 @@ void invalidCommand()
     printerror("Invalid command, enter \"help\" for all available commands \n");
 }
 
-void printerror(char* errmsg)
+void printerror(char *errmsg)
 {
     printf("\033[1;31m");
-    printf("%s\a",errmsg);
+    printf("%s\a", errmsg);
     printf("\033[0m");
+}
+
+void grepUtil()
+{
+    char *argv[1024];
+    char *token;
+    int i = 0;
+    token = strtok(command, " ");
+    while (token != NULL)
+    {
+        argv[i] = token;
+        token = strtok(NULL, " ");
+        i++;
+    }
+    argv[i] = NULL;
+    if (argv[1] == NULL)
+    {
+        printerror("Invalid command, enter \"help\" for all available commands \n");
+    }
+    else
+    {
+        char *fileName = argv[2];
+        char *pattern = argv[1];
+        char *line = NULL;
+        size_t len = 0;
+        ssize_t read;
+        FILE *fp = fopen(fileName, "r");
+        if (fp == NULL)
+        {
+            printerror("File not found \n");
+            printf("Usage: grep pattern fileName \n");
+        }
+        else
+        {
+            while ((read = getline(&line, &len, fp)) != -1)
+            {
+                if (strstr(line, pattern) != NULL)
+                {
+                    printf("%s", line);
+                }
+            }
+            fclose(fp);
+            if (line)
+                free(line);
+        }
+    }
+}
+
+void removeFile()
+{
+    char *fileName = strtok(command, " ");
+    fileName = strtok(NULL, " ");
+    if (fileName == NULL)
+    {
+        printerror("Invalid command, enter \"help\" for all available commands \n");
+    }
+    else
+    {
+        if (remove(fileName) != 0)
+        {
+            printerror("Error removing file \n");
+        }
+        else
+        {
+            printf("File removed successfully \n");
+        }
+    }
 }
 
 void cdCommand()
@@ -106,7 +191,7 @@ void cdCommand()
             else
             {
                 printf("\033[1;31m");
-                printf("%s",token);
+                printf("%s", token);
                 printf("\033[0m");
                 printerror(" is not a directory\n");
             }
@@ -114,7 +199,7 @@ void cdCommand()
         else
         {
             printf("\033[1;31m");
-            printf("%s",token);
+            printf("%s", token);
             printf("\033[0m");
             printerror(" does not exist\n");
         }
@@ -124,23 +209,23 @@ void cdCommand()
 void listCommand()
 {
     int priv = 0, inode = 0, time = 0;
-    if(strlen(command)>6 || strlen(command) ==4)
+    if (strlen(command) > 6 || strlen(command) == 4)
     {
-        for(int i=6;i<strlen(command); i++)
+        for (int i = 6; i < strlen(command); i++)
         {
-            if(command[i]=='p')
+            if (command[i] == 'p')
             {
                 priv = 1;
             }
-            else if(command[i]=='n')
+            else if (command[i] == 'n')
             {
                 inode = 1;
             }
-            else if(command[i]=='t')
+            else if (command[i] == 't')
             {
                 time = 1;
             }
-            else if(command[i]=='a')
+            else if (command[i] == 'a')
             {
                 priv = 1;
                 time = 1;
@@ -170,13 +255,39 @@ void listCommand()
     chdir(".");
     while ((entry = readdir(dp)) != NULL)
     {
-        if(strcmp(entry->d_name,".")==0||strcmp(entry->d_name,"..")==0)
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
             continue;
         lstat(entry->d_name, &statbuf);
-        printf("%-12s\t", entry->d_name);
-        if(priv)
+        if (!S_ISLNK(statbuf.st_mode))
+            printf("%-12s\t", entry->d_name);
+        else
+            printf("\033[1;32m%-12s\033[0m", entry->d_name);
+        if (priv)
         {
-            printf((S_ISDIR(statbuf.st_mode)) ? "d" : "-");
+            // check if file is directory or fifo or link
+            if (S_ISDIR(statbuf.st_mode))
+            {
+                printf("d");
+            }
+            else if (S_ISFIFO(statbuf.st_mode))
+            {
+                printf("p");
+            }
+            else if (S_ISLNK(statbuf.st_mode))
+            {
+                // print the link of the file
+                char linkname[1024];
+                readlink(entry->d_name, linkname, sizeof(linkname));
+                // extract the file name from the link
+                char *token = strtok(linkname, "/");
+                // print the link
+                printf("->%s\t", token);
+                printf("l");
+            }
+            else
+            {
+                printf("-");
+            }
             printf((statbuf.st_mode & S_IRUSR) ? "r" : "-");
             printf((statbuf.st_mode & S_IWUSR) ? "w" : "-");
             printf((statbuf.st_mode & S_IXUSR) ? "x" : "-");
@@ -187,20 +298,214 @@ void listCommand()
             printf((statbuf.st_mode & S_IWOTH) ? "w" : "-");
             printf((statbuf.st_mode & S_IXOTH) ? "x\t" : "-\t");
         }
-        if(inode)
+        if (inode)
         {
             printf("%ld\t\t", entry->d_ino);
         }
-        if(time)
+        if (time)
         {
             struct stat attr;
             stat(entry->d_name, &attr);
             printf("%s", ctime(&(attr.st_mtime)));
         }
-        if(!time)
-            printf("\n");  
+        if (!time)
+            printf("\n");
     }
     closedir(dp);
+}
+
+void makelink()
+{
+    // makelink might have some bugs so dont combine it with main.c
+    int i = 0, k = 1, t = 1;
+    struct stat st;
+    char file1[100], file2[100], n_command[1024];
+    strcpy(n_command, command);
+    int sym = 0;
+    if (command[5] == '-')
+    {
+        for (int i = 6; i < 7; i++)
+        {
+            if (command[i] == 's')
+            {
+                sym = 1;
+            }
+        }
+    }
+    char *checker = strtok(n_command, " ");
+    while (checker != NULL)
+    {
+        t++;
+        checker = strtok(NULL, " ");
+    }
+
+    if (t < 3)
+    {
+        invalidCommand();
+    }
+
+    char *tokens = strtok(command, " ");
+    if (sym == 1)
+    {
+        while (tokens != NULL)
+        {
+            if (tokens == "link" || tokens == "-s")
+            {
+                continue;
+            }
+            if (k == 3)
+            {
+                strcpy(file1, tokens);
+            }
+            else if (k == 4)
+            {
+                strcpy(file2, tokens);
+            }
+            k++;
+            tokens = strtok(NULL, " ");
+        }
+    }
+    else if (sym == 0)
+    {
+        while (tokens != NULL)
+        {
+            if (tokens == "link" || tokens == "-s")
+            {
+                continue;
+            }
+            if (k == 2)
+            {
+                strcpy(file1, tokens);
+            }
+            else if (k == 3)
+            {
+                strcpy(file2, tokens);
+            }
+            k++;
+            tokens = strtok(NULL, " ");
+        }
+    }
+
+    if (access(file1, F_OK))
+    {
+        perror("ERROR: filename not found\n");
+        return;
+    }
+    if (!access(file2, F_OK))
+    {
+        perror("ERROR: filename already exists\n");
+        return;
+    }
+    if (stat(file1, &st) < 0)
+    {
+        perror("ERROR: unable to get stat info\n");
+        return;
+    }
+    if (!S_ISREG(st.st_mode))
+    {
+        perror("ERROR: not a Regular File");
+        return;
+    }
+
+    if (sym == 0)
+    {
+        if (link(file1, file2) < 0)
+        {
+            perror("ERROR: unable to create the Link");
+            return;
+        }
+        else
+        {
+            printf("Link created between %s and %s\n", file1, file2);
+            return;
+        }
+    }
+
+    else if (sym == 1)
+    {
+        if (symlink(file1, file2) < 0)
+        {
+            perror("symlink");
+            return;
+        }
+        else
+        {
+            printf("Symbolic link created between %s and %s\n", file1, file2);
+            return;
+        }
+    }
+}
+
+void removelink()
+{
+    int i = 0, k = 1, t = 1;
+    struct stat st;
+    char file1[100], file2[100], n_command[1024];
+    strcpy(n_command, command);
+    int sym = 0;
+    char *checker = strtok(n_command, " ");
+    while (checker != NULL)
+    {
+        t++;
+        checker = strtok(NULL, " ");
+    }
+
+    if (t < 3)
+    {
+        invalidCommand();
+    }
+
+    char *tokens = strtok(command, " ");
+    if (sym == 0)
+    {
+        while (tokens != NULL)
+        {
+            if (tokens == "unlink")
+            {
+                continue;
+            }
+            if (k == 2)
+            {
+                strcpy(file1, tokens);
+            }
+            else if (k == 3)
+            {
+                strcpy(file2, tokens);
+            }
+            k++;
+            tokens = strtok(NULL, " ");
+        }
+    }
+
+    if (access(file1, F_OK))
+    {
+        perror("ERROR: linkname not found\n");
+        return;
+    }
+    if (stat(file1, &st) < 0)
+    {
+        perror("ERROR: unable to get stat info\n");
+        return;
+    }
+    if (!S_ISREG(st.st_mode))
+    {
+        perror("ERROR: not a Regular File");
+        return;
+    }
+
+    if (sym == 0)
+    {
+        if (unlink(file1) < 0)
+        {
+            perror("ERROR: unable to create the Link");
+            return;
+        }
+        else
+        {
+            printf("Unlinked %s\n", file1);
+            return;
+        }
+    }
 }
 
 // function to print the documentation for the project by reading CLIHelp.txt
--- test2.c
+++ test2.c
@@ -10,7 +10,7 @@
 char command[1024];
 
 void invalidCommand();
-void printerror(char* errmsg);
+void printerror(char *errmsg);
 void cdCommand();
 void listCommand();
 void helpCommand();
@@ -23,7 +23,7 @@ void main()
     printf("Command line interface started! Enter \"help\" for all available commands");
     printf("\033[0m");
     printf("\n\n");
-    //get current working directory
+    // get current working directory
     char cwd[1024];
     char hostname[1024];
     do
@@ -35,7 +35,7 @@ void main()
         printf("%s@%s:", getlogin(), hostname);
         printf("\033[0m");
         printf("\033[0;34;1m");
-        printf("\x1b[1m"); 
+        printf("\x1b[1m");
         printf("%s> ", cwd);
         printf("\x1b[0m");
         printf("\033[0m");
@@ -49,11 +49,11 @@ void main()
         {
             helpCommand();
         }
-        else if (strncmp(command, "link",4) == 0 || strncmp(command, "link -",6)==0)
+        else if (strncmp(command, "link", 4) == 0 || strncmp(command, "link -", 6) == 0)
         {
             makelink();
         }
-        else if (strncmp(command, "unlink",6) == 0)
+        else if (strncmp(command, "unlink", 6) == 0)
         {
             removelink();
         }
@@ -61,7 +61,7 @@ void main()
         {
             system("clear");
         }
-        else if(strcmp(command, "clr") == 0)
+        else if (strcmp(command, "clr") == 0)
         {
             system("clear");
         }
@@ -70,9 +70,8 @@ void main()
         {
             cdCommand();
         }
-        else if(strcmp(command, "") == 0)
+        else if (strcmp(command, "") == 0)
         {
-
         }
         else
         {
@@ -87,10 +86,10 @@ void invalidCommand()
     printerror("Invalid command, enter \"help\" for all available commands \n");
 }
 
-void printerror(char* errmsg)
+void printerror(char *errmsg)
 {
     printf("\033[1;31m");
-    printf("%s\a",errmsg);
+    printf("%s\a", errmsg);
     printf("\033[0m");
 }
 
@@ -116,7 +115,7 @@ void cdCommand()
             else
             {
                 printf("\033[1;31m");
-                printf("%s",token);
+                printf("%s", token);
                 printf("\033[0m");
                 printerror(" is not a directory\n");
             }
@@ -124,7 +123,7 @@ void cdCommand()
         else
         {
             printf("\033[1;31m");
-            printf("%s",token);
+            printf("%s", token);
             printf("\033[0m");
             printerror(" does not exist\n");
         }
@@ -134,23 +133,23 @@ void cdCommand()
 void listCommand()
 {
     int priv = 0, inode = 0, time = 0;
-    if(strlen(command)>6 || strlen(command) ==4)
+    if (strlen(command) > 6 || strlen(command) == 4)
     {
-        for(int i=6;i<strlen(command); i++)
+        for (int i = 6; i < strlen(command); i++)
         {
-            if(command[i]=='p')
+            if (command[i] == 'p')
             {
                 priv = 1;
             }
-            else if(command[i]=='n')
+            else if (command[i] == 'n')
             {
                 inode = 1;
             }
-            else if(command[i]=='t')
+            else if (command[i] == 't')
             {
                 time = 1;
             }
-            else if(command[i]=='a')
+            else if (command[i] == 'a')
             {
                 priv = 1;
                 time = 1;
@@ -180,11 +179,11 @@ void listCommand()
     chdir(".");
     while ((entry = readdir(dp)) != NULL)
     {
-        if(strcmp(entry->d_name,".")==0||strcmp(entry->d_name,"..")==0)
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
             continue;
         lstat(entry->d_name, &statbuf);
         printf("%-12s\t", entry->d_name);
-        if(priv)
+        if (priv)
         {
             printf((S_ISDIR(statbuf.st_mode)) ? "d" : "-");
             printf((statbuf.st_mode & S_IRUSR) ? "r" : "-");
@@ -197,179 +196,215 @@ void listCommand()
             printf((statbuf.st_mode & S_IWOTH) ? "w" : "-");
             printf((statbuf.st_mode & S_IXOTH) ? "x\t" : "-\t");
         }
-        if(inode)
+        if (inode)
         {
             printf("%ld\t\t", entry->d_ino);
         }
-        if(time)
+        if (time)
         {
             struct stat attr;
             stat(entry->d_name, &attr);
             printf("%s", ctime(&(attr.st_mtime)));
         }
-        if(!time)
-            printf("\n");  
+        if (!time)
+            printf("\n");
     }
     closedir(dp);
 }
 
-void makelink(){
-    //makelink might have some bugs so dont combine it with main.c
-    int i=0,k=1,t=1;
+void makelink()
+{
+    // makelink might have some bugs so dont combine it with main.c
+    int i = 0, k = 1, t = 1;
     struct stat st;
-    char file1[100],file2[100],n_command[1024];
-    strcpy(n_command,command);
-    int sym=0;
-    if(command[5]=='-'){
-    for(int i=6;i<7; i++){
-        if(command[i]=='s'){
-            sym = 1;
+    char file1[100], file2[100], n_command[1024];
+    strcpy(n_command, command);
+    int sym = 0;
+    if (command[5] == '-')
+    {
+        for (int i = 6; i < 7; i++)
+        {
+            if (command[i] == 's')
+            {
+                sym = 1;
+            }
         }
-     }
-   }
+    }
     char *checker = strtok(n_command, " ");
-    while( checker != NULL ) {
+    while (checker != NULL)
+    {
         t++;
-      checker = strtok(NULL, " ");
-     }
+        checker = strtok(NULL, " ");
+    }
 
-    if(t<3){
+    if (t < 3)
+    {
         invalidCommand();
     }
 
     char *tokens = strtok(command, " ");
-    if(sym==1){
-    while(tokens!=NULL){
-        if(tokens=="link" || tokens=="-s"){
-            continue;
+    if (sym == 1)
+    {
+        while (tokens != NULL)
+        {
+            if (tokens == "link" || tokens == "-s")
+            {
+                continue;
+            }
+            if (k == 3)
+            {
+                strcpy(file1, tokens);
+            }
+            else if (k == 4)
+            {
+                strcpy(file2, tokens);
+            }
+            k++;
+            tokens = strtok(NULL, " ");
         }
-        if(k==3){
-            strcpy(file1,tokens);
-        }
-        else if(k==4){
-            strcpy(file2,tokens);
-        }
-        k++;
-        tokens = strtok(NULL, " ");
     }
-  }
-    else if(sym==0){
-        while(tokens!=NULL){
-        if(tokens=="link" || tokens=="-s"){
-            continue;
+    else if (sym == 0)
+    {
+        while (tokens != NULL)
+        {
+            if (tokens == "link" || tokens == "-s")
+            {
+                continue;
+            }
+            if (k == 2)
+            {
+                strcpy(file1, tokens);
+            }
+            else if (k == 3)
+            {
+                strcpy(file2, tokens);
+            }
+            k++;
+            tokens = strtok(NULL, " ");
         }
-        if(k==2){
-            strcpy(file1,tokens);
-        }
-        else if(k==3){
-            strcpy(file2,tokens);
-        }
-        k++;
-        tokens = strtok(NULL, " ");
-    }
     }
 
-    if(access(file1,F_OK)){
+    if (access(file1, F_OK))
+    {
         perror("ERROR: filename not found\n");
         return;
     }
-    if(!access(file2,F_OK)){
+    if (!access(file2, F_OK))
+    {
         perror("ERROR: filename already exists\n");
         return;
     }
-    if(stat(file1,&st)<0){
+    if (stat(file1, &st) < 0)
+    {
         perror("ERROR: unable to get stat info\n");
         return;
     }
-    if(!S_ISREG(st.st_mode)){
+    if (!S_ISREG(st.st_mode))
+    {
         perror("ERROR: not a Regular File");
-        return; 
+        return;
     }
 
-    if(sym==0){
-        if(link(file1,file2)<0){
+    if (sym == 0)
+    {
+        if (link(file1, file2) < 0)
+        {
             perror("ERROR: unable to create the Link");
             return;
+        }
+        else
+        {
+            printf("Link created between %s and %s\n", file1, file2);
+            return;
+        }
     }
-    else{
-        printf("Link created between %s and %s\n",file1,file2);
-        return;
-    }
-    }
-    
-    else if(sym==1){
-        if(symlink(file1,file2)<0){
-             perror("ERROR: unable to create the Link");
-             return;
-    }
-    else{
-        printf("Symbolic link created between %s and %s\n",file1,file2);
-        return;
-    }
-    }
-   
 
+    else if (sym == 1)
+    {
+        if (symlink(file1, file2) < 0)
+        {
+            perror("symlink");
+            return;
+        }
+        else
+        {
+            printf("Symbolic link created between %s and %s\n", file1, file2);
+            return;
+        }
+    }
 }
 
-void removelink(){
-    int i=0,k=1,t=1;
+void removelink()
+{
+    int i = 0, k = 1, t = 1;
     struct stat st;
-    char file1[100],file2[100],n_command[1024];
-    strcpy(n_command,command);
-    int sym=0;
+    char file1[100], file2[100], n_command[1024];
+    strcpy(n_command, command);
+    int sym = 0;
     char *checker = strtok(n_command, " ");
-    while( checker != NULL ) {
+    while (checker != NULL)
+    {
         t++;
-      checker = strtok(NULL, " ");
-     }
+        checker = strtok(NULL, " ");
+    }
 
-    if(t<3){
+    if (t < 3)
+    {
         invalidCommand();
     }
 
     char *tokens = strtok(command, " ");
-    if(sym==0){
-        while(tokens!=NULL){
-        if(tokens=="unlink"){
-            continue;
+    if (sym == 0)
+    {
+        while (tokens != NULL)
+        {
+            if (tokens == "unlink")
+            {
+                continue;
+            }
+            if (k == 2)
+            {
+                strcpy(file1, tokens);
+            }
+            else if (k == 3)
+            {
+                strcpy(file2, tokens);
+            }
+            k++;
+            tokens = strtok(NULL, " ");
         }
-        if(k==2){
-            strcpy(file1,tokens);
-        }
-        else if(k==3){
-            strcpy(file2,tokens);
-        }
-        k++;
-        tokens = strtok(NULL, " ");
-    }
     }
 
-    if(access(file1,F_OK)){
+    if (access(file1, F_OK))
+    {
         perror("ERROR: linkname not found\n");
         return;
     }
-    if(stat(file1,&st)<0){
+    if (stat(file1, &st) < 0)
+    {
         perror("ERROR: unable to get stat info\n");
         return;
     }
-    if(!S_ISREG(st.st_mode)){
+    if (!S_ISREG(st.st_mode))
+    {
         perror("ERROR: not a Regular File");
-        return; 
-    }
-
-    if(sym==0){
-        if(unlink(file1)<0){
-            perror("ERROR: unable to create the Link");
-            return;
-    }
-    else{
-        printf("Unlinked %s\n",file1);
         return;
     }
+
+    if (sym == 0)
+    {
+        if (unlink(file1) < 0)
+        {
+            perror("ERROR: unable to create the Link");
+            return;
+        }
+        else
+        {
+            printf("Unlinked %s\n", file1);
+            return;
+        }
     }
-    
 }
-   
 
 // function to print the documentation for the project by reading CLIHelp.txt
 void helpCommand()
